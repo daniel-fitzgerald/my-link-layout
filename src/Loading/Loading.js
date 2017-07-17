0e58308240b71070c8b791b8ed48843635da9eb0
daniel-fitzgerald <Fitzgerald Daniel@agdaff.gov.au>
2017-07-17 15:57:54 +1000

    change loading page

--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -4,23 +4,26 @@ import { Link } from 'react-router'
 
 import Messages from '../Messages'
 
+import Content from '../Content'
+
 import './loading.css'
 
-function Content({ messages }) {
-    if (messages) {
-        return <Messages messages={messages} />
-    } else {
-        return <p>Loading...</p>
-    }
-}
+// function Content({ messages }) {
+//     if (messages) {
+//         return <Messages messages={messages} />
+//     } else {
+//         return <p>Loading...</p>
+//     }
+// }
 
 function Loading({ children, messages }) {
-    return <div>
-        <div className="main">
-            <Content messages={messages} />
-            {children}
-        </div>
-    </div>
+    let title = 'Loading'
+    if (messages) {
+        title = ''
+    }
+    return <Content messages={messages} title={title} breadcrumbs={{currentLabel: title}}>
+        {children}
+    </Content>
 }
 
 export default Loading
